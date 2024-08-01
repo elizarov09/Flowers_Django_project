84342452edb87488ccaeee3d8e1011dee51ba245
elizarov09 <elizarov09@gmail.com>
2024-08-01 12:26:33 +0300

    2_main_and_register_form

---
+++ flowers_project/main/urls.py
@@ -0,0 +1,8 @@
+# main/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
---
+++ flowers_project/main/views.py
@@ -0,0 +1,6 @@
+# main/views.py
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
